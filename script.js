ecd18a5e0d834d314c95ab41e0f52c6ecb3df1d8
thao15059 <thao15059@gmail.com>
2021-06-05 22:07:31 +0700

    implements controls 3d on iphone image

--- script.js
+++ script.js
@@ -32,3 +32,57 @@ const slideshow = () => {
 };
 
 slideshow();
+
+let x = 0;
+let y = 0;
+let z = 0;
+let isHover = true;
+let interval;
+
+const cube = document.querySelector(".cube");
+
+document.querySelector(".top-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".bottom-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".left-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".right-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".top-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg)`;
+});
+
+document.querySelector(".bottom-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg)`;
+});
+
+const playPause = () => {
+  if (isHover) {
+    interval = setInterval(() => {
+      cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
+    }, 100);
+  } else {
+    clearInterval(interval);
+  }
+};
+
+// playPause();
+
+document.querySelector(".controls").addEventListener("mouseover", () => {
+  isHover = !isHover;
+  playPause();
+});
+
+document.querySelector(".controls").addEventListener("mouseout", () => {
+  isHover = !isHover;
+  playPause();
+});
